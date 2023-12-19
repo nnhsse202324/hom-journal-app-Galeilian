de489b0013018887a1e2e71a8e1324d12a1a3692
“Galeilian <wddyhxdyx@126.com>
2023-12-19 08:41:18 -0600

    1我

--- server.js
+++ server.js
@@ -2,3 +2,26 @@
  * main Javascript file for the application
  *  this file is executed by the Node server
  */
+
+// import the express module, which exports the express function
+const express = require("express");
+
+// invoke the express function to create an Express application
+const app = express();
+
+// set the template engine to EJS, which generates HTML with embedded JavaScript
+app.set("view engine", "ejs");
+
+// load assets
+app.use("/css", express.static("assets/css"));
+app.use("/img", express.static("assets/img"));
+app.use("/js", express.static("assets/js"));
+
+// to keep this file manageable, we will move the routes to a separate file
+//  the exported router object is an example of middleware
+app.use("/", require("./server/routes/router"));
+
+// start the server on port 8080
+app.listen(8080, () => {
+  console.log("server is listening on http://localhost:8080");
+});
